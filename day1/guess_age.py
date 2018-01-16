f74df37843f73dafb8087771cd18c4ae74d32a6a
alex <iduhao38426@163.com>
2018-01-16 22:30:57 +0800

    edit
    继续优化

--- day1/guess_age.py
+++ day1/guess_age.py
@@ -1,11 +1,15 @@
 # Author:Alex Du
 My_age = 25
-Guess_age = int(input("Please guess my age:"))
-if Guess_age == My_age:
-    print("Yes,You got it !!!")
-elif Guess_age < My_age:
-    print("Yahoo....You can guess a bigger...")
+count = 1
+while count < 3 :
+    Guess_age = int(input("Please guess my age:"))
+    if Guess_age == My_age:
+        print("Yes,You got it !!!")
+        break
+    elif Guess_age < My_age:
+        print("Yahoo....You can guess a bigger...")
+    else:
+        print("Em.....You should  think a smaller...")
+        count = count + 1
 else:
-    print("Em.....You should  think a smaller...")
-
-print("hello,world")
+    print("you have tried too many times..stop it")
