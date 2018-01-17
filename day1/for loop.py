f57536b321c0e5051b3134eb45180c73bfe4fd03
alex <iduhao38426@163.com>
2018-01-17 22:36:41 +0800

    edit

---
+++ day1/for loop.py
@@ -0,0 +1,5 @@
+# Author:Alex Du
+#!/usr/bin/env python
+
+for i in range(0,10,2):
+    print("loop",i)
--- day1/guess_age_for_loop.py
+++ day1/guess_age_for_loop.py
@@ -1,7 +1,7 @@
 # Author:Alex Du
 My_age = 25
-count = 1
-while count < 4 :
+
+for i in range(10):
     Guess_age = int(input("Please guess my age:"))
     if Guess_age == My_age:
         print("Yes,You got it !!!")
@@ -10,6 +10,5 @@ while count < 4 :
         print("Yahoo....You can guess a bigger...")
     else:
         print("Em.....You should  think a smaller...")
-        count = count + 1
 else:
     print("you have tried too many times..stop it")
