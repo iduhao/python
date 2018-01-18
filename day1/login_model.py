5ca946ac5be14e65f433ed0208e16f9343d1ab11
alex <iduhao38426@163.com>
2018-01-18 21:54:33 +0800

    creat files
    creat file edit login model.test getpass model

---
+++ day1/login_model.py
@@ -0,0 +1,5 @@
+# Author:Alex Du
+import  getpass
+user = input("please set username:")
+pwd = getpass.getpass("Please set your password:")
+print("Your username is ", user + ", Your password is:", pwd)
